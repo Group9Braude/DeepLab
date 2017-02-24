287a64f7880a19fbc1afdf2d72c588583d9cdd74
orels <orels@DESKTOP-8NSO5LH>
2017-02-24 22:51:15 +0200

    Entities halfly done

---
+++ DeepSiamLab/src/Entities/CCR.java
@@ -0,0 +1,29 @@
+package Entities;
+
+public class CCR {
+private String serialNum, manufacturer, owner;
+
+public String getSerialNum() {
+	return serialNum;
+}
+
+public void setSerialNum(String serialNum) {
+	this.serialNum = serialNum;
+}
+
+public String getManufacturer() {
+	return manufacturer;
+}
+
+public void setManufacturer(String manufacturer) {
+	this.manufacturer = manufacturer;
+}
+
+public String getOwner() {
+	return owner;
+}
+
+public void setOwner(String owner) {
+	this.owner = owner;
+}
+}
